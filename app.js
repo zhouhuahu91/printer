e4abdbd36e74c42016d6c2395aef4951ccb9f2ba
zhouhuahu91 <Zhouhua.hu@gmail.com>
2024-01-26 20:18:23 +0100

    added a http server that listens to print

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const express = require("express");
 // Imports from for the printer to connect to Epson printer
 const { ThermalPrinter, PrinterTypes } = require("node-thermal-printer");
-
+// This functions turns the order into a png receipt
 const createReceipt = require("./createReceipt");
+
+const app = express();
+app.use(express.json()); // Middleware to parse JSON bodies
 
 // Initializes the printer.
 let printer = new ThermalPrinter({
@@ -9,13 +13,36 @@ let printer = new ThermalPrinter({
   interface: "/dev/usb/lp0",
 });
 
-const print = async () => {
-  const pngBuffer = createReceipt();
-  // Print the PNG.
-  await printer.printImageBuffer(pngBuffer);
-  printer.cut();
+app.post("/print", async (req, res) => {
+  try {
+    const order = req.body; // The JSON object sent by the user is in req.body
+    const receipt = createReceipt(order);
+    // Check if printer is connected
+    let isConnected = await printer.isPrinterConnected();
+    // If connected we start printing.
+    if (isConnected) {
+      await printer.printImageBuffer(receipt);
+      printer.cut();
+      // We execute the print.
+      const status = await printer.execute();
+      if (status) {
+        // If print is succes we respond with order printed.
+        res.send("order printed");
+      } else {
+        // If print failed we throw error
+        throw new Error("Failed to execute print command");
+      }
+    } else {
+      // If Printer is not connected we throw error
+      throw new Error("Printer not connected");
+    }
+  } catch (e) {
+    // Handle any errors that occur during processing
+    res.status(500).send("Something went wrong: " + e.message);
+  }
+});
 
-  printer.execute();
-};
-
-print();
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
