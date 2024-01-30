0022725993c662cb4261107b6cf0a0759991e400
Zhouhua Hu <zhouhua.hu@gmail.com>
2024-01-30 19:12:58 +0100

    added an process array for orders being processed

--- app.js
+++ app.js
@@ -7,67 +7,90 @@ const { ThermalPrinter, PrinterTypes } = require("node-thermal-printer");
 const createReceipt = require("./createReceipt.js");
 
 (async () => {
-  console.log("Printer is now running");
+  try {
+    console.log("Printer is now running");
 
-  const q = db
-    .collection("orders")
-    .where("printed", "==", false)
-    .where("isPrinting", "==", true);
+    const q = db
+      .collection("orders")
+      .where("printed", "==", false)
+      .where("isPrinting", "==", true);
 
-  q.onSnapshot((snapshot) => {
-    const data = snapshot.docs.map((doc) => {
-      return { ...doc.data(), id: doc.id };
-    });
-
-    // We loop over each order that hasn't been printed yet but is in the process of printing.
-    data.forEach(async (order) => {
-      // We get the snapshot if this order.
-      const ref = db.doc(`orders/${order.id}`);
-      const orderSnapshot = await ref.get();
-
-      // We order doesn't exist we return from this order.
-      if (orderSnapshot.exists === false) return; // Just in case order get deleted while in this process.
-
-      // We initiate the printer.
-      let printer = new ThermalPrinter({
-        type: PrinterTypes.EPSON,
-        interface: "/dev/usb/lp0",
+    q.onSnapshot((snapshot) => {
+      const data = snapshot.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
       });
-      // We check if the printer is connected
-      let isConnected = await printer.isPrinterConnected();
 
-      if (isConnected === false) {
-        // If not connected we set order back to not printing
-        await ref.update({
-          isPrinting: false,
+      // We loop over each order that hasn't been printed yet but is in the process of printing.
+      data.forEach(async (order) => {
+        // This array contains orders currently being printed.
+        let ordersBeingProcessed = [];
+        ordersBeingProcessed.push(order.id);
+
+        // We get the snapshot if this order.
+        const ref = db.doc(`orders/${order.id}`);
+        const orderSnapshot = await ref.get();
+
+        // We order doesn't exist we return from this order.
+        if (orderSnapshot.exists === false) {
+          ordersBeingProcessed = ordersBeingProcessed.filter(
+            (id) => id !== order.id
+          );
+          return console.log("order does not exist");
+        } // Just in case order get deleted while in this process.
+
+        // We initiate the printer.
+        let printer = new ThermalPrinter({
+          type: PrinterTypes.EPSON,
+          interface: "/dev/usb/lp0",
         });
-        return console.log("Printer is not connected.");
-      }
+        // We check if the printer is connected
+        let isConnected = await printer.isPrinterConnected();
 
-      // ***** HERE WE ACTUALLY PRINT THE ORDER ****
-      // If we reach this far we can set isPrinting back to false
-      // This is safer in case we print the order more than once.
-      await ref.update({
-        isPrinting: false,
+        if (isConnected === false) {
+          // If not connected we set order back to not printing
+          await ref.update({
+            isPrinting: false,
+          });
+          // We remove the order from the process array.
+          ordersBeingProcessed = ordersBeingProcessed.filter(
+            (id) => id !== order.id
+          );
+          return console.log("Printer is not connected.");
+        }
+
+        // ***** HERE WE ACTUALLY PRINT THE ORDER ****
+
+        // First we need the receipt
+        const receipt = await createReceipt(order);
+
+        // Then print the receipt and wait for response
+        printer.printImageBuffer(receipt);
+        printer.cut();
+        const status = await printer.execute();
+
+        // If status is good we update isPrinting to false and printed to true
+        if (status) {
+          await ref.update({
+            isPrinting: false,
+            printed: true,
+          });
+          // If printed and printed is being set we remove the order from the process array.
+          ordersBeingProcessed = ordersBeingProcessed.filter(
+            (id) => id !== order.id
+          );
+          // Something went wrong and we only set isPrinting back to false
+        } else {
+          await ref.update({
+            isPrinting: false,
+          });
+          // And we remove the id from processing.
+          ordersBeingProcessed = ordersBeingProcessed.filter(
+            (id) => id !== order.id
+          );
+        }
       });
-      // First we need the receipt
-      const receipt = await createReceipt(order);
-
-      // Then print the receipt and wait for response
-      printer.printImageBuffer(receipt);
-      printer.cut();
-      const status = await printer.execute();
-
-      // If status is good we update printed to true
-      if (status) {
-        await ref.update({
-          printed: true,
-        });
-
-        // Something went wrong and we only set isPrinting back to false
-      } else {
-        console.log("Something went wrong with printing");
-      }
     });
-  });
+  } catch (e) {
+    console.log(e.message);
+  }
 })();
